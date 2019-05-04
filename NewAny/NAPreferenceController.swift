c8808551e58faa28ed91e19413a84031a7e8bcdd
Yue Yang <g1enyy0ung@gmail.com>
2019-05-04 19:35:19 +0800

    Finish all features

--- NewAny/NAPreferenceController.swift
+++ NewAny/NAPreferenceController.swift
@@ -6,19 +6,22 @@
 //  Copyright © 2019 ilime. All rights reserved.
 //
 
+import os.log
 import Cocoa
 
 class NAPreferenceController: NSViewController, NSTableViewDataSource {
     
-    private let d = UserDefaults.standard
-    
+    static let log = OSLog(subsystem: "org.ilime.NewAny", category: "NAPreferenceController")
+    private let NewAnyFolderURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Library/Containers/org.ilime.NewAny.NewAnyFinder/Data/NewAnyFolder")
     @IBOutlet weak var addFileTextField: NSTextField!
     @IBOutlet weak var filesTable: NSTableView!
     
     func initFileList() {
-        let files = d.array(forKey: "NAFileList")
-        if files == nil {
-            d.set([], forKey: "NAFileList")
+        if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+            let files = d.array(forKey: "NAFileList")
+            if files == nil {
+                d.set([], forKey: "NAFileList")
+            }
         }
     }
     
@@ -27,26 +30,61 @@ class NAPreferenceController: NSViewController, NSTableViewDataSource {
         
         filesTable.delegate = self
         filesTable.dataSource = self
+        
         initFileList()
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return d.array(forKey: "NAFileList")?.count ?? 0
+        if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+            return d.array(forKey: "NAFileList")?.count ?? 0
+        }
+        
+        return 0
     }
     
     @IBAction func addFile(_ sender: NSButton) {
         let filename = addFileTextField.stringValue
-        var files = d.array(forKey: "NAFileList")!
+        if (filename.isEmpty) {
+            return
+        }
         
-        files.append(["name": filename])
-        d.set(files, forKey: "NAFileList")
-        
-        reloadFileList()
-        
-        addFileTextField.stringValue = ""
+        if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+            var files = d.array(forKey: "NAFileList")!
+            files.append(["name": filename, "exist": "N"])
+            d.set(files, forKey: "NAFileList")
+            
+            reloadFileList()
+            
+            addFileTextField.stringValue = ""
+        }
     }
     
     @IBAction func importFile(_ sender: NSButton) {
+        let panel = NSOpenPanel()
+        
+        panel.canChooseFiles = true
+        panel.canChooseDirectories = false
+        panel.allowsMultipleSelection = false
+        panel.canCreateDirectories = false
+        
+        if panel.runModal() == .OK {
+            if let url = panel.url {
+                do {
+                    try FileManager.default.copyItem(at: url, to: NewAnyFolderURL.appendingPathComponent(url.lastPathComponent))
+                    
+                    if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+                        var files = d.array(forKey: "NAFileList")!
+                        files.append(["name": url.lastPathComponent, "exist": "Y"])
+                        d.set(files, forKey: "NAFileList")
+                        
+                        reloadFileList()
+                    }
+                } catch let error {
+                    let message = "Error occuring in importFile(): \(error)"
+                    os_log("%@", log: NAPreferenceController.log, type: .error, message)
+                }
+            }
+        }
     }
     
     func reloadFileList() {
@@ -56,19 +94,22 @@ class NAPreferenceController: NSViewController, NSTableViewDataSource {
 
 extension NAPreferenceController: NSTableViewDelegate {
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        let files = d.array(forKey: "NAFileList")
-        
-        guard let file = files?[row] as? [String: String] else {
-            return nil
-        }
-        
-        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "NameCellID"), owner: nil) as? NSTableCellView {
-            cell.textField?.stringValue = file["name"]!
-            return cell
+        if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+            let files = d.array(forKey: "NAFileList")
+            
+            guard let file = files?[row] as? [String: String] else {
+                return nil
+            }
+            
+            if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "NameCellID"), owner: nil) as? NSTableCellView {
+                cell.textField?.stringValue = file["name"]!
+                return cell
+            }
         }
         
         return nil
     }
+    
     
     override func rightMouseDown(with event: NSEvent) {
         let menu = NSMenu()
@@ -77,12 +118,32 @@ extension NAPreferenceController: NSTableViewDelegate {
     }
     
     @objc private func deleteFile(_ sender: AnyObject) {
-        guard filesTable.clickedRow >= 0 else { return }
-
-        var files = d.array(forKey: "NAFileList")!
-        files.remove(at: filesTable.clickedRow)
-        d.set(files, forKey: "NAFileList")
+        guard !filesTable.selectedRowIndexes.isEmpty else {
+            return
+        }
         
-        reloadFileList()
+        if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+            var files = d.array(forKey: "NAFileList") as! [[String: String]]
+            filesTable
+                .selectedRowIndexes
+                .sorted(by: >)
+                .forEach { index in
+                    if files[index]["exist"] == "Y" {
+                        let filename = files[index]["name"]!
+                        
+                        do {
+                            try FileManager.default.removeItem(at: NewAnyFolderURL.appendingPathComponent(filename))
+                        } catch let error {
+                            let message = "Error occuring in deleteFile(): \(error)"
+                            os_log("%@", log: NAPreferenceController.log, type: .error, message)
+                        }
+                    }
+                    
+                    files.remove(at: index)
+            }
+            d.set(files, forKey: "NAFileList")
+            
+            reloadFileList()
+        }
     }
 }
--- NewAnyFinder/FinderSync.swift
+++ NewAnyFinder/FinderSync.swift
@@ -6,19 +6,33 @@
 //  Copyright © 2019 ilime. All rights reserved.
 //
 
+import os.log
 import Cocoa
 import FinderSync
 
 class FinderSync: FIFinderSync {
     
-    private let d = UserDefaults.standard
-    let NewAnyFolderURL = URL(fileURLWithPath: "/Users/Shared/NewAny")
+    static let log = OSLog(subsystem: "org.ilime.NewAny.NewAnyFinder", category: "FinderSync")
+    private let NewAnyFolderURL = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("NewAnyFolder")
     
     override init() {
         super.init()
         
         // Set up the directory we are syncing.
-        FIFinderSyncController.default().directoryURLs = [self.NewAnyFolderURL]
+        FIFinderSyncController.default().directoryURLs = [NewAnyFolderURL]
+        
+        checkNewAnyFolder()
+    }
+    
+    func checkNewAnyFolder() {
+        if !FileManager.default.fileExists(atPath: NewAnyFolderURL.path) {
+            do {
+                try FileManager.default.createDirectory(at: NewAnyFolderURL, withIntermediateDirectories: false, attributes: nil)
+            } catch let error {
+                let message = "Error occuring in checkNewAnyFolder(): \(error)"
+                os_log("%@", log: FinderSync.log, type: .error, message)
+            }
+        }
     }
     
     // MARK: - Menu and toolbar item support
@@ -37,16 +51,57 @@ class FinderSync: FIFinderSync {
     
     override func menu(for menuKind: FIMenuKind) -> NSMenu {
         let menu = NSMenu(title: "NewAny")
-        let files = d.array(forKey: "NAFileList") as! [[String : String]]
         
-        for file in files {
-            menu.addItem(withTitle: file["name"]!, action: #selector(createFile(_:)), keyEquivalent: "")
+        if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+            let files = d.array(forKey: "NAFileList") as! [[String : String]]
+            
+            for file in files {
+                menu.addItem(withTitle: file["name"]!, action: #selector(createFile(_:)), keyEquivalent: "")
+            }
         }
         
         return menu
     }
     
-    @objc private func createFile(_ sender: AnyObject?) {
+    @objc private func createFile(_ item: NSMenuItem) {
+        let filename = item.title
         
+        if let d = UserDefaults(suiteName: "group.org.ilime.NewAny.SharedData") {
+            var files = d.array(forKey: "NAFileList") as! [[String : String]]
+            var selectedFile: [String: String] = [:]
+            
+            for file in files {
+                if file["name"] == filename {
+                    selectedFile = file
+                    break
+                }
+            }
+            
+            if !selectedFile.isEmpty {
+                if let to = FIFinderSyncController.default().targetedURL()?.appendingPathComponent(filename) {
+                    do {
+                        let at = NewAnyFolderURL.appendingPathComponent(filename)
+                        
+                        if selectedFile["exist"] == "N" {
+                            try "".write(to: at, atomically: false, encoding: .utf8)
+                            try FileManager.default.copyItem(at: at, to: to)
+                            
+                            for i in 0..<files.count {
+                                if files[i]["name"] == filename {
+                                    files[i]["exist"] = "Y"
+                                }
+                            }
+                            
+                            d.set(files, forKey: "NAFileList")
+                        } else if selectedFile["exist"] == "Y" {
+                            try FileManager.default.copyItem(at: at, to: to)
+                        }
+                    } catch let error {
+                        let message = "Error occuring in createFile(): \(error)"
+                        os_log("%@", log: FinderSync.log, type: .error, message)
+                    }
+                }
+            }
+        }
     }
 }
